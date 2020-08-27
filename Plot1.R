10c42dba3fa007b084be4813c33956e4a93277b0
bentafoya <btafoya@comcast.net>
2020-08-27 14:12:03 -0400

    Add files via upload

--- Plot1.R
+++ Plot1.R
@@ -1,20 +1,26 @@
+## Download and setup file
+
 downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 downloadFile <- "./household_power_consumption.zip"
 householdFile <- "./household_power_consumption.txt"
-householdFile <- "./household_power_consumption.txt"
-##
+
+## Check to make sure file exists
 if (!file.exists(householdFile)) {
   download.file(downloadURL, downloadFile, method = "curl")
   unzip(downloadFile, overwrite = T, exdir = "./Data")
 }
-##
+
+## Read and clean the table
 plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
-## set time variable
+## set time interval variable
 finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
 SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 finalData <- cbind(SetTime, finalData)
-## Generating Plot 2
+
+## Generating Plot 1
 hist(finalData$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
 ## Copy Plot1 to file and close
 dev.copy(png, file = "Plot1.png")
 dev.off()
+
--- Plot3.R
+++ Plot3.R
@@ -1,18 +1,21 @@
+## Download and setup file
+
 downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 downloadFile <- "./household_power_consumption.zip"
 householdFile <- "./household_power_consumption.txt"
-householdFile <- "./household_power_consumption.txt"
-##
+
+## Check to make sure file exists
 if (!file.exists(householdFile)) {
   download.file(downloadURL, downloadFile, method = "curl")
   unzip(downloadFile, overwrite = T, exdir = "./Data")
 }
-##
+## Read and clean the table
 plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
-## set time variable
+## set time interval variable
 finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
 SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 finalData <- cbind(SetTime, finalData)
+
 ## Generating Plot 3
 columnlines <- c("black", "red", "blue")
 labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
@@ -20,6 +23,7 @@ plot(finalData$SetTime, finalData$Sub_metering_1, type="l", col=columnlines[1], 
 lines(finalData$SetTime, finalData$Sub_metering_2, col=columnlines[2])
 lines(finalData$SetTime, finalData$Sub_metering_3, col=columnlines[3])
 legend("topright", legend=labels, col=columnlines, lty="solid")
+
 ## Copy Plot3 to file and close
 dev.copy(png, file = "Plot3.png")
 dev.off()
